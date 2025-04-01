3fee9ce1644d7b93864b9f44f20d841eef5967d1
Quentin <Quentin.cheron26200@hotmail.com>
2025-04-01 14:54:41 +0200

    update style

--- src/app/(frontend)/blog/page.tsx
+++ src/app/(frontend)/blog/page.tsx
@@ -1,10 +1,13 @@
 import InitFilter from '@/components/init-filter'
+import { Suspense } from 'react'
 
 export default async function BlogsPage() {
   return (
     <div className="max-w-6xl mx-auto px-4 pb-8">
       <h1 className="text-center text-4xl font-bold mb-12 text-gray-800">Explorez nos Articles</h1>
-      <InitFilter />
+      <Suspense fallback={<div>Loading...</div>}>
+        <InitFilter />
+      </Suspense>
     </div>
   )
 }
